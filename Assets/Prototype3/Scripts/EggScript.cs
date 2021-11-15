fcb0aa8a2bbbfcdaa61248116012924f5bf1848e
pompom2600 <ogonzalez1@myjmc.edu.au>
2021-11-15 18:26:57 +1100

    Week8
    Math

---
+++ Assets/Prototype3/Scripts/EggScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggScript : MonoBehaviour
+{
+    TiltControl tiltControl;
+
+    private void Start()
+    {
+        tiltControl = FindObjectOfType<TiltControl>();
+    }
+
+
+    void Update()
+    {
+        if (transform.position.y < -10)
+        {
+            Debug.Log(transform.position.y);
+            tiltControl.SpawnEgg();
+            Destroy(gameObject);
+        }
+
+    }
+}
